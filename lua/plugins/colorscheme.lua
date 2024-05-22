6c7041900b56112995ebaac30e35f5c91562bc7c
qerplunk <aquinoedgar@outlook.com>
2024-05-22 02:26:15 -0700

    Remove italics from kanagawa theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,18 @@
 return {
     {
         "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({
+                commentStyle = { italic = false },
+                keywordStyle = { italic = false },
+
+                overrides = function()
+                    return {
+                        ["@variable.builtin"] = { italic = false },
+                    }
+                end,
+            })
+        end,
     },
     {
         "olivercederborg/poimandres.nvim",
