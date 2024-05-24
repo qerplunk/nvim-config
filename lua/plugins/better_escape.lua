6f55622ea5b64bb5ed09e76267045b490521b179
qerplunk <aquinoedgar@outlook.com>
2024-05-23 18:51:50 -0700

    Unnamed plus clipboard makes WSL past and delete slow

--- lua/plugins/better_escape.lua
+++ lua/plugins/better_escape.lua
@@ -1,10 +1,9 @@
 return {
     "max397574/better-escape.nvim",
     config = function()
-        require("better_escape").setup {
+        require("better_escape").setup({
             mapping = { "jk" },
             timeout = 250,
-        }
+        })
     end,
-
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -10,11 +10,11 @@ vim.opt.cmdheight = 0
 local space = " "
 vim.opt.list = true
 vim.opt.listchars:append({
-	tab = "│ ",
-	multispace = space,
-	lead = space,
-	trail = space,
-	nbsp = space,
+    tab = "│ ",
+    multispace = space,
+    lead = space,
+    trail = space,
+    nbsp = space,
 })
 
 vim.opt.tabstop = 4
@@ -51,20 +51,19 @@ vim.opt.writebackup = false
 vim.opt.updatetime = 300
 vim.opt.signcolumn = "yes"
 
-vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 -- Highlight current line only on current bufffer
 vim.opt.cursorline = true -- Highlight the current line
 local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
 local set_cursorline = function(event, value, pattern)
-	vim.api.nvim_create_autocmd(event, {
-		group = group,
-		pattern = pattern,
-		callback = function()
-			vim.opt_local.cursorline = value
-		end,
-	})
+    vim.api.nvim_create_autocmd(event, {
+        group = group,
+        pattern = pattern,
+        callback = function()
+            vim.opt_local.cursorline = value
+        end,
+    })
 end
 
 set_cursorline("WinLeave", false)
